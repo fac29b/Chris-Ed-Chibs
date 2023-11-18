b0cab65b827e53820ec61525e2f6db0b9ac81629
Chibu <chibsdev@gmail.com>
2023-11-18 11:32:17 +0000

    api input field created

--- chat.js
+++ chat.js
@@ -1,3 +1,5 @@
+let localApiKey;
+
 function displayText() {
     // Get the text from the textarea
     var inputText = document.getElementById('inputText').value;
@@ -8,3 +10,23 @@ function displayText() {
     // Set the text content of the display area
     displayArea.textContent = inputText;
   }
+
+
+  // api-key.js
+
+function storeApiKey() {
+  
+  const apiKeyInput = document.getElementById('apiKey');
+  const apiKey = apiKeyInput.value;
+
+  // Validate apiKey if needed
+
+  // Store in a local variable
+   localApiKey = apiKey;
+
+  // Display a warning for learning purposes
+  console.warn('This API key is stored in a local variable for learning purposes only. Do not use in production.');
+
+  // You can now use the localApiKey variable in your application
+  console.log('API Key stored:', localApiKey);
+}
