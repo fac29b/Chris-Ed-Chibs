537054e765847e5bf242e1577a9464e9440e5408
Edga380 <a.edgaras44@gmail.com>
2023-11-18 14:53:04 +0000

    Final update

--- chat.js
+++ chat.js
@@ -1,4 +1,9 @@
 const apiUrl = "https://api.openai.com/v1/chat/completions";
+// Variables
+let submitMessageClass;
+let textToDisplay;
+let userMessage = [{ role: 'user', content: "Can you welcome me in the style of a friendly north of england style and ask if you can help me?" }];
+let apiKeyStored;
 
 async function fetchData(apiUrl, apiKey) {
   try {
@@ -10,7 +15,7 @@ async function fetchData(apiUrl, apiKey) {
           },
           body: JSON.stringify({
               model: 'gpt-3.5-turbo',
-              messages: [{ role: 'user', content: 'You are a helpful assistant.' }],
+              messages: userMessage,
               stream: false,
           }),
       });
@@ -20,9 +25,12 @@ async function fetchData(apiUrl, apiKey) {
       }
 
       const data = await response.json();
+      userMessage.push({ role: 'system', content: data.choices[0].message.content });
+      DisplayChatGPTMessage(data.choices[0].message.content);
       //Close log in if you get data
       closeLoginModal();
       console.log("Data:", data);
+
 
   } catch (error) {
       console.error("Error:", error);
@@ -45,10 +53,36 @@ function login() {
   //document.getElementById("apiKeyInput").value;
   console.log(document.getElementById("apiKeyInput").value);
   // For simplicity, let's just close the modal for now
-  fetchData(apiUrl, document.getElementById("apiKeyInput").value);
+  apiKeyStored = document.getElementById("apiKeyInput").value;
+  fetchData(apiUrl, apiKeyStored);
 }
 
 // Show the login modal when the page loads (for demonstration purposes)
 window.onload = function() {
+  submitMessageClass = document.querySelector(".message-input");
+  textToDisplay = document.querySelector(".chat-container")
   showLoginModal();
 };
+
+// Submit your message
+function SubmitMessage(){
+  userMessage.push({ role: 'user', content: submitMessageClass.value });
+  fetchData(apiUrl, document.getElementById("apiKeyInput").value);
+  textToDisplay.insertAdjacentHTML('beforeend', `
+  <div class="chat-message user-message">
+      <div class="message-content">`
+          + submitMessageClass.value +
+      `</div>
+</div>`);
+}
+
+
+// display chatGPT message
+function DisplayChatGPTMessage(text){
+  textToDisplay.insertAdjacentHTML('beforeend', `
+  <div class="chat-message assistant-message">
+      <div class="message-content">`
+          + text +
+      `</div>
+</div>`);
+}
