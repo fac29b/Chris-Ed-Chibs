e16279b683a97f0dbc0f48d9fd6b24f5f7d6310c
Edga380 <a.edgaras44@gmail.com>
2023-11-18 11:43:23 +0000

    Api connection

--- chat.js
+++ chat.js
@@ -30,3 +30,30 @@ function storeApiKey() {
   // You can now use the localApiKey variable in your application
   console.log('API Key stored:', localApiKey);
 }
+
+function fetchData(apiEndpoint, apiKey) {
+  const headers = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}` // Include your API key here
+  });
+
+  return fetch(apiEndpoint, { headers })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+          }
+          return response.json();
+      });
+}
+
+// Example usage
+const apiUrl = "https://api.openai.com/v1/chat/completions"; // Replace with your API endpoint
+const apiKey = "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"; // Replace with your actual API key
+fetchData(apiUrl, apiKey)
+  .then(data => {
+      console.log("Data:", data);
+      // Process the fetched data as needed
+  })
+  .catch(error => {
+      console.error("Error:", error);
+  });
